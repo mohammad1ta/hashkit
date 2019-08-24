c43ae2cd667cd19784b43ed8ada4efbaac257d47
m.yektamaram <mohammad.1ta@gmail.com>
2019-08-24 17:47:39 +0430

    Init package

---
+++ utility/src/Utility.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Hashkit\Utility;
+
+class Utility
+{
+
+    public static function encode( $value ) {
+
+        return $value;
+
+    }
+
+    public static function decode( $value ) {
+
+        return $value;
+
+    }
+
+}
